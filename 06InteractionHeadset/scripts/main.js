ed148ec08d910e5b07877ada34e0110dec1ee5a2
Milou de Koning <miloudekoning@MacBook-Pro-van-Milou.local>
2020-05-17 13:50:50 +0200

    alles /2

--- 06InteractionHeadset/scripts/main.js
+++ 06InteractionHeadset/scripts/main.js
@@ -3,8 +3,8 @@ var rotationSpeed = 0.001;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	myOtherBox.object3D.rotation.x += rotationSpeed;
-	myOtherBox.object3D.rotation.y += rotationSpeed;
+	myOtherBox.object3D.rotation.x += rotationSpeed/2;
+	myOtherBox.object3D.rotation.y += rotationSpeed/2;
 	myOtherBox.object3D.rotation.z += rotationSpeed/2;
 	//console.log(myOtherBox.object3D.rotation);
 }
